5787624d32250e5c057e0da60006946b511e0990
fuwei <454575238@qq.com>
2022-01-22 18:46:39 +0800

    feat: add 110-medium-capitalize

---
+++ type-challenges/110-medium-capitalize/template.ts
@@ -0,0 +1,38 @@
+type WordMap = {
+  a: 'A'
+  b: 'B'
+  c: 'C'
+  d: 'D'
+  e: 'E'
+  f: 'F'
+  g: 'G'
+  h: 'H'
+  i: 'I'
+  j: 'J'
+  k: 'K'
+  l: 'L'
+  m: 'M'
+  n: 'N'
+  o: 'O'
+  p: 'P'
+  q: 'Q'
+  r: 'R'
+  s: 'S'
+  t: 'T'
+  u: 'U'
+  v: 'V'
+  w: 'W'
+  x: 'X'
+  y: 'Y'
+  z: 'Z'
+}
+
+type MyUpperCase<S extends string> = S extends keyof WordMap
+  ? WordMap[S]
+  : S
+
+type MyCapitalize<S extends string> = S extends `${infer R}${infer Rest}`
+  ? `${MyUpperCase<R>}${Rest}`
+  : S
+
+
