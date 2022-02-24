c4d2eb791e97572978b18fab939214238796e270
fuwei <454575238@qq.com>
2022-02-24 19:41:52 +0800

    refactor: 3188-medium-tuple-to-nested-object

--- type-challenges/3188-medium-tuple-to-nested-object/template.ts
+++ type-challenges/3188-medium-tuple-to-nested-object/template.ts
@@ -1,32 +1,50 @@
-type CorrectVal<T, U> = [keyof T] extends [never] ? U : T
+// type CorrectVal<T, U> = [keyof T] extends [never] ? U : T
 
-type TupleToNestedObject<T, U, R = {}> = T extends [...infer Rest, infer F]
-  ? F extends string
-    ? Rest extends []
-      ? { [key in F]: CorrectVal<R, U> }
-      : TupleToNestedObject<
-          Rest,
-          U,
-          {
-            [key in F]: CorrectVal<R, U>
-          }
-        >
-    : U
-  : U
+// type TupleToNestedObject<T, U, R = {}> = T extends [...infer Rest, infer F]
+//   ? F extends string
+//     ? Rest extends []
+//       ? { [key in F]: CorrectVal<R, U> }
+//       : TupleToNestedObject<
+//           Rest,
+//           U,
+//           {
+//             [key in F]: CorrectVal<R, U>
+//           }
+//         >
+//     : U
+//   : U
 
-// 这里是js的解答
-function tupleToNestObject(
-  target: string[],
-  val: unknown,
-  result: Record<string, any> = {}
-) {
-  if (!target.length) return val
+// // 这里是js的解答
+// function tupleToNestObject(
+//   target: string[],
+//   val: unknown,
+//   result: Record<string, any> = {}
+// ) {
+//   if (!target.length) return val
+//   const [first, ...rest] = target
+//   if (rest.length > 0) {
+//     result[first] = {}
+//     tupleToNestObject(rest, val, result[first])
+//   } else {
+//     result[first] = val
+//   }
+//   return result
+// }
+
+// 好吧 再一次发现了更好的方法 妈的 // 他们说这就是人生
+// 学到了 key in F & string 这种操作
+
+type TupleToNestedObject<T, U> = T extends [infer F, ...infer R] ? {
+  [key in F & string]: TupleToNestedObject<R, U>
+} : U
+
+// 这里是对应的js解法
+function tupleToNestedObject(target: string[], val: unknown): unknown {
+  if(target.length === 0) return val
   const [first, ...rest] = target
-  if (rest.length > 0) {
-    result[first] = {}
-    tupleToNestObject(rest, val, result[first])
-  } else {
-    result[first] = val
+  return {
+    [first]: tupleToNestedObject(rest, val)
   }
-  return result
 }
+
+console.log(tupleToNestedObject(['a', 'b'], 4234));
