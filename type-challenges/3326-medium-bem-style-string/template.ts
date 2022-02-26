9ce4f8501712cc32defae69cbcc5caaab1d3efc5
fuwei <454575238@qq.com>
2022-02-26 17:02:44 +0800

    feat: 3326-medium-bem-style-string

--- type-challenges/3326-medium-bem-style-string/template.ts
+++ type-challenges/3326-medium-bem-style-string/template.ts
@@ -4,6 +4,7 @@ type BEM<
   M extends string[]
 > = `${B}${SplitStringBySign<E, '__'>}${SplitStringBySign<M, '--'>}`
 
+// 其实可以用`${T[number]}`来获取字符串的分割
 type SplitStringBySign<T, U = '', S = never> = T extends [infer F, ...infer R]
   ? SplitStringBySign<R, U, `${U & string}${F & string}` | S>
   : [S] extends [never]
