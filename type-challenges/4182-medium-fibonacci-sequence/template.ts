4acf7ef5ad74dfc03e188650b0e33a46660b5e33
fuwei <454575238@qq.com>
2022-03-14 18:17:46 +0800

    feat: add 4182-medium-fibonacci-sequence

---
+++ type-challenges/4182-medium-fibonacci-sequence/template.ts
@@ -0,0 +1,79 @@
+type Unit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+
+type MakeArray<
+  T extends Unit,
+  S extends unknown[] = []
+> = T extends `${S['length']}` ? S : MakeArray<T, [...S, 0]>
+
+type MakeArray10<T extends unknown[]> = [
+  ...T,
+  ...T,
+  ...T,
+  ...T,
+  ...T,
+  ...T,
+  ...T,
+  ...T,
+  ...T,
+  ...T
+]
+
+type _ToArray<
+  T extends string | number,
+  R extends unknown[] = []
+> = `${T}` extends `${infer P}${infer K}`
+  ? P extends Unit
+    ? _ToArray<K, [...MakeArray10<R>, ...MakeArray<P>]>
+    : never
+  : R
+
+type _Minus<T extends number, N extends number> = ToArray<T> extends [
+  ...ToArray<N>,
+  ...infer R
+]
+  ? R['length']
+  : 0
+
+type Add<T extends number, N extends number> = [
+  ...ToArray<N>,
+  ...ToArray<T>
+]['length']
+
+type Fibonacci<
+  T extends number,
+  U extends any[] = [any],
+  V = 1,
+  P = 1
+> = U extends { length: T }
+  ? V
+  : Fibonacci<T, [any, ...U], P, Add<V & number, P & number>>
+
+
+type a <T extends number, U extends unknown[] = [unknown], V = 1, P = 1> = U extends {length: T} ? V : a<T, [unknown, ...U], P,  Add<V & number, P & number>>
+
+function _makeArray(len: number) {
+  if (len < 10) return new Array(len).fill(0)
+  return []
+}
+
+function make10(arr: unknown[]) {
+  return [
+    ...arr,
+    ...arr,
+    ...arr,
+    ...arr,
+    ...arr,
+    ...arr,
+    ...arr,
+    ...arr,
+    ...arr,
+    ...arr,
+  ]
+}
+function dec(target: string, result: unknown[] = []): unknown[] {
+  const strArr = target.split('')
+  if (!strArr.length) return result
+  const firstNum = Number(strArr.shift())
+
+  return dec(strArr.join(''), [...make10(result), ..._makeArray(firstNum)])
+}
